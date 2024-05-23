f652d9345efefed866bd740119b7b42c49be036b
David Rodríguez <deivid.rodriguez@riseup.net>
2024-05-23 19:43:13 +0200

    More

--- bundler/spec/install/gems/dependency_api_fallback_spec.rb
+++ bundler/spec/install/gems/dependency_api_fallback_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "../support/silent_logger"
+require_relative "../../support/silent_logger"
 
 RSpec.describe "gemcutter's dependency API" do
   context "when Gemcutter API takes too long to respond" do
@@ -10,7 +10,7 @@ RSpec.describe "gemcutter's dependency API" do
       port = find_unused_port
       @server_uri = "http://127.0.0.1:#{port}"
 
-      require_relative "../support/artifice/endpoint_timeout"
+      require_relative "../../support/artifice/endpoint_timeout"
 
       @t = Thread.new do
         server = Rack::Server.start(app: EndpointTimeout,
@@ -36,11 +36,11 @@ RSpec.describe "gemcutter's dependency API" do
     it "times out and falls back on the modern index" do
       install_gemfile <<-G, artifice: nil, env: { "BUNDLER_SPEC_GEM_REPO" => gem_repo1.to_s }
         source "#{@server_uri}"
-        gem "rack"
+        gem "myrack"
       G
 
       expect(out).to include("Fetching source index from #{@server_uri}/")
-      expect(the_bundle).to include_gems "rack 1.0.0"
+      expect(the_bundle).to include_gems "myrack 1.0.0"
     end
   end
 end
